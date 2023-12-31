126ac4614fce3976dba62bbfe99a9100103d3ea9
schanzerman <62482706+schanzerman@users.noreply.github.com>
2023-12-31 17:09:05 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,11 @@
+# Set the directory path where the CSV files are located
+directory = "G:/My Drive/PhD/DATA/Photocatalysis/230118-TiO2_trial"
+directory = "G:/My Drive/PhD/DATA/Photocatalysis/230119-03530-DAB-56"
+directory = "G:/My Drive/PhD/DATA/Photocatalysis/230119-TiO2"
+directory = "G:/My Drive/PhD/DATA/Photocatalysis/230125-03530-DAB-56-redone"
+directory = "G:/My Drive/PhD/DATA/Photocatalysis/230125-TiO2-redone"
+directory = "G:/My Drive/PhD/DATA/Photocatalysis/230131-03543-A-TPA56"
+directory = "G:/My Drive/PhD/DATA/Photocatalysis/230131-03548-A-TPA-28"
+directory = "G:/My Drive/PhD/DATA/Photocatalysis/230208-TiO2_reredone"
+
+sample = directory.split('/')[-1]
